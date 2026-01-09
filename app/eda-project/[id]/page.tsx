db83fde6a9743fa599f8554041f5dd7ec473e15a
Asbeel24 <1721443003@qq.com>
2026-01-10 00:33:49 +0800

    Add empty lines to multiple files for improved readability and organization

--- app/eda-project/[id]/page.tsx
+++ app/eda-project/[id]/page.tsx
@@ -47,3 +47,4 @@ export default function EDAProjectDetail({ params }: { params: Promise<{ id: str
   );
 }
 
+
--- components/SafeImage.tsx
+++ components/SafeImage.tsx
@@ -27,3 +27,4 @@ export default function SafeImage({ src, alt, className = '' }: SafeImageProps) 
 
 
 
+
